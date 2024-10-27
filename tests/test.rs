91a7c3f98a8405515bceb53a2ae2c47c4e4821cf
David Tolnay <dtolnay@gmail.com>
2024-10-27 11:40:39 -0700

    Add HashSet test

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,5 @@
 use erased_discriminant::Discriminant;
+use std::collections::HashSet;
 
 enum Enum<'a> {
     A(#[allow(dead_code)] &'a str),
@@ -23,4 +24,21 @@ fn test_eq() {
     let different_discriminant = Discriminant::of(&DifferentEnum::A);
     assert_ne!(a_discriminant, different_discriminant);
     assert_ne!(b_discriminant, different_discriminant);
+}
+
+#[test]
+fn test_hashset() {
+    let mut set = HashSet::new();
+
+    let temporary_string = "...".to_owned();
+    set.insert(Discriminant::of(&Enum::A(&temporary_string)));
+    set.insert(Discriminant::of(&Enum::B));
+    set.insert(Discriminant::of(&DifferentEnum::A));
+    drop(temporary_string);
+    assert_eq!(set.len(), 3);
+
+    set.insert(Discriminant::of(&Enum::A("other string")));
+    set.insert(Discriminant::of(&Enum::B));
+    set.insert(Discriminant::of(&DifferentEnum::A));
+    assert_eq!(set.len(), 3);
 }
