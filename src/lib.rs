97a76392fd9e888b91a47346156e672caf0a2559
David Tolnay <dtolnay@gmail.com>
2024-10-27 13:59:51 -0700

    Add crosslink icons to top of rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! [![github]](https://github.com/dtolnay/erased-discriminant)&ensp;[![crates-io]](https://crates.io/crates/erased-discriminant)&ensp;[![docs-rs]](https://docs.rs/erased-discriminant)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+//!
 //! This crate provides a `Discriminant` type that behaves like
 //! `core::mem::Discriminant<T>` but without the generic type parameter `T`.
 //! With this, we can build collections such as HashSet that contain
